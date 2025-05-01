5fb479b7c3f1a4733c67d2f0c954d77cddbba581
omairasdaq <abortboy@gmail.com>
2025-05-01 21:11:09 +0500

    Update lexicalanalyser.py

--- lexicalanalyser.py
+++ lexicalanalyser.py
@@ -4,8 +4,11 @@ import sys
 class Lexer:
     def __init__(self, text):
         self.text = text
-        self.tokenNames = ["OPEN_PARENS", "CLOSE_PARENS", "PLUS", "MINUS", "TIMES", "DIVIDE", "POWER", "NUMBER", "FUNCTION", "COMMA"]
-        self.numOrSymbol = re.compile(r'(\d+\.\d+|\d+|[a-zA-Z_][a-zA-Z_0-9]*|[^\s\d\w])')  # match numbers, functions, or any non-space, non-digit, non-word character
+        self.tokenNames = [
+            "OPEN_PARENS", "CLOSE_PARENS", "PLUS", "MINUS", "TIMES", "DIVIDE",
+            "POWER", "NUMBER", "FUNCTION", "COMMA", "IDENTIFIER"
+        ]
+        self.numOrSymbol = re.compile(r'(\d+\.\d+|\d+|[a-zA-Z_][a-zA-Z_0-9]*|[^\s\d\w])')
         self.tokens = []
         self.errors = []
 
@@ -13,25 +16,30 @@ class Lexer:
         self.tokenize = re.findall(self.numOrSymbol, self.text)
         for tok in self.tokenize:
             if tok == '(':
-                self.tokens.append([tok, self.tokenNames[0]])  # OPEN_PARENS
+                self.tokens.append([tok, "OPEN_PARENS"])
             elif tok == ')':
-                self.tokens.append([tok, self.tokenNames[1]])  # CLOSE_PARENS
+                self.tokens.append([tok, "CLOSE_PARENS"])
             elif tok == '+':
-                self.tokens.append([tok, self.tokenNames[2]])  # PLUS
+                self.tokens.append([tok, "PLUS"])
             elif tok == '-':
-                self.tokens.append([tok, self.tokenNames[3]])  # MINUS
+                self.tokens.append([tok, "MINUS"])
             elif tok == '*':
-                self.tokens.append([tok, self.tokenNames[4]])  # TIMES
+                self.tokens.append([tok, "TIMES"])
             elif tok == '/':
-                self.tokens.append([tok, self.tokenNames[5]])  # DIVIDE
+                self.tokens.append([tok, "DIVIDE"])
             elif tok == '^':
-                self.tokens.append([tok, self.tokenNames[6]])  # POWER
+                self.tokens.append([tok, "POWER"])
             elif tok == ',':
-                self.tokens.append([tok, self.tokenNames[9]])  # COMMA
-            elif tok.isnumeric() or re.match(r'\d+\.\d+', tok):  # For floating point numbers as well
-                self.tokens.append([tok, self.tokenNames[7]])  # NUMBER
-            elif tok in ["sin", "cos", "tan", "log", "sqrt", "abs", "pow", "acos", "asin", "atan", "exp"]:  # Add functions here
-                self.tokens.append([tok, self.tokenNames[8]])  # FUNCTION
+                self.tokens.append([tok, "COMMA"])
+            elif tok.isnumeric() or re.match(r'^\d+\.\d+$', tok):
+                self.tokens.append([tok, "NUMBER"])
+            elif tok in [
+                "sin", "cos", "tan", "log", "sqrt", "abs", "pow",
+                "acos", "asin", "atan", "exp", "log10"
+            ]:
+                self.tokens.append([tok, "FUNCTION"])
+            elif re.match(r'^[a-zA-Z_][a-zA-Z_0-9]*$', tok):
+                self.tokens.append([tok, "IDENTIFIER"])
             else:
                 self.errors.append(tok)
 
@@ -41,4 +49,3 @@ class Lexer:
             print("Error! Found unknown tokens.")
             print(self.errors)
             sys.exit()
-
